57cf6d5a508286f59fa7ee7db592186495591645
elesc888 <elesc888@gmail.com>
2024-09-25 01:37:50 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export interface Proxy {
 export let PROXYS = [ 
     {
         domain: "easy-reverse-proxy-pied.vercel.app",
-        url: "https://3sim.net/",
+        url: "https://3api.xyz/",
         enable: true
     }
 ];
