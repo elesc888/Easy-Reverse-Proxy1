323c555c0e3f1edf8f97c07b00c0bf4ca93a9432
elesc888 <elesc888@gmail.com>
2024-09-25 01:15:59 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export interface Proxy {
 export let PROXYS = [ 
     {
         domain: "easy-reverse-proxy-pied.vercel.app",
-        url: "http://3api.xyz/",
+        url: "https://www.google.com/",
         enable: true
     }
 ];
